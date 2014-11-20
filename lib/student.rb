41b0fa0ef1bea09df9eec8fc1622e3e031483e74
Jerome Pratt <jerome.pratt@outlook.com>
2014-11-20 17:49:26 +0000

    refactoring - taking method out of Student#initialize

--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,28 @@
 class Student 
-	attr_accessor :name, :cohort, :hobby
+    attr_accessor :name, :cohort, :hobby
 
-	def initialize(details = {})
+    def initialize(details = {})
         @name = details[:name] || "no name given"
+        set_instance_variables(details)
+    end
+
+    def method_missing(method, string = nil )
+        method_string = method.to_s ; method_sliced = method_string.slice(4..-1)
+        if method_has_add_ = (method_string.to_s.slice(0..3) == "add_")
+            instance_variable_set("@#{method_sliced}", string)
+            self.class.send(:attr_accessor, method_sliced)
+        else super
+        end
+    end
+
+    private
+    def set_instance_variables(details)
         details.each {|key, value|
             if detail = details[key]
                 instance_variable_set("@#{key.to_s}", detail) 
                 self.class.send(:attr_accessor, key)
             end
         }
-	end
-
-	def method_missing(method, string = nil )
-        method_string = method.to_s ; method_sliced = method_string.slice(4..-1)
-		if method_has_add_ = (method_string.to_s.slice(0..3) == "add_")
-			instance_variable_set("@#{method_sliced}", string)
-			self.class.send(:attr_accessor, method_sliced)
-		else super
-		end
-	end
+    end
 
 end
